6536b01922a998a64cfb785a0081484742191b3e
JosueDurand <josue.durand8@gmail.com>
2019-07-11 02:30:06 -0500

    sincronizar 5%

--- app/src/main/java/amazoniaresilientes/durand/josue/amazoniaresiliente/Sincronizar/AdapterSincronizar.java
+++ app/src/main/java/amazoniaresilientes/durand/josue/amazoniaresiliente/Sincronizar/AdapterSincronizar.java
@@ -1,7 +1,9 @@
 package amazoniaresilientes.durand.josue.amazoniaresiliente.Sincronizar;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +14,13 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import amazoniaresilientes.durand.josue.amazoniaresiliente.Inicio.PhotoActivity;
+import amazoniaresilientes.durand.josue.amazoniaresiliente.Inicio.RegisterClienteActivity;
 import amazoniaresilientes.durand.josue.amazoniaresiliente.Login.LoginActivity;
 import amazoniaresilientes.durand.josue.amazoniaresiliente.R;
 import amazoniaresilientes.durand.josue.amazoniaresiliente.Room.Ntch_Amazonia;
 import amazoniaresilientes.durand.josue.amazoniaresiliente.Room.SincronizarListAdapter;
+import androidx.appcompat.app.AlertDialog;
 
 public class AdapterSincronizar extends BaseAdapter {
     private Context context;
@@ -67,15 +72,71 @@ public class AdapterSincronizar extends BaseAdapter {
             holder = (AdapterSincronizar.ViewHolder) row.getTag();
         }
 
-        Ntch_Amazonia food = foodsList.get(position);
-        final String id = food.getId();
+        Ntch_Amazonia amazonia = foodsList.get(position);
+        //Datos a sincronizar
+        final String id = amazonia.getId();
+        final String cultivo = amazonia.getCultivo();
+        final String primer_nombre = amazonia.getPrimer_nombre();
+        final String segundo_nombre = amazonia.getSegundo_nombre();
+        final String apellido_paterno =amazonia.getApellido_paterno();
+        final String apellido_materno = amazonia.getApellido_materno();
+        final String estado_civil = amazonia.getEstado_civil();
+        final String dni = amazonia.getDni();
+        final String referencia_predio = amazonia.getReferencia_predio();
+        final String departamento_cliente = amazonia.getDepartamento_cliente();
+        final String poligono = amazonia.getPoligono();
+        final String area = amazonia.getArea();
+        byte[] imagen1 = amazonia.getImagen1();
+        final String lat1;
+        final String lng1;
+        byte[] imagen2 = amazonia.getImagen2();
+        final  String lat2;
+        final  String lng2;
+        byte[] imagen3 = amazonia.getImagen3();
+        final String lat3;
+        final String lng3;
+        byte[] imagen4 = amazonia.getImagen4();
+        final String lat4;
+        final String lng4;
 
-        holder.txtName.setText(food.getPrimer_nombre()+" "+food.getApellido_paterno());
-        holder.txtPrice.setText(food.getSegundo_nombre());
+
+
+        holder.txtName.setText(amazonia.getPrimer_nombre()+" "+amazonia.getApellido_paterno());
+        holder.txtPrice.setText(amazonia.getDni());
         holder.imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 Toast.makeText(context, "Esperando..."+id+" ok", Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getRootView().getContext());
+                builder.setTitle("App");
+                builder.setMessage("¿Desea continuar registrando los datos");
+                builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Esperando..."+id+" ok", Toast.LENGTH_SHORT).show();
+                        try{
+
+
+//                            Intent intent = new Intent(PhotoActivity.this, SincronizarPoligono.class);
+//                            startActivity(intent);
+
+                            // imageView.setImageResource(R.mipmap.ic_launcher);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        // finish();
+
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
              //   Intent in = new Intent(context, LoginActivity.class);
               //  context.startActivity(in);
 
