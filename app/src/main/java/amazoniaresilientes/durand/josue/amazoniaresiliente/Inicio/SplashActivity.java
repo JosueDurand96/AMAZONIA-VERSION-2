dfdf2585defdc6b97a3f52bc88c0fd0c3719c2fe
JosueDurand <josue.durand8@gmail.com>
2019-07-25 08:16:10 -0500

    splash

--- app/src/main/java/amazoniaresilientes/durand/josue/amazoniaresiliente/Inicio/SplashActivity.java
+++ app/src/main/java/amazoniaresilientes/durand/josue/amazoniaresiliente/Inicio/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run(){
                 // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
                 //Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                Intent intent = new Intent(SplashActivity.this, MapaActivity.class);
+                Intent intent = new Intent(SplashActivity.this, RegisterClienteActivity.class);
                 startActivity(intent);
                 finish();
             }
